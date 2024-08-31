26e32bfaba30a32cd2772217a9d30a3f9bc58902
Gary James <gtjames@gmail.com>
2024-08-31 12:08:41 -0500

    Update hello.c added a bang!

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main (int argc, char* argv[]) {
     if ( argc > 1)
-        printf("Hello %s", argv[1]);
+        printf("Hello %s!", argv[1]);
     else
-        printf("Hello World");
+        printf("Hello World!");
 }
